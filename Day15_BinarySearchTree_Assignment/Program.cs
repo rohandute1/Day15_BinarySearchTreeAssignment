e65e35f87aa0f142493c32cfb6ee8df944dc8dfa
Rohan <rohandute1@gmail.com>
2023-06-20 19:08:47 +0530

    [Rohan] Successfully added welcome message

---
+++ Day15_BinarySearchTree_Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_BinarySearchTree_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Assignment");
+            Console.ReadLine();
+        }
+    }
+}
